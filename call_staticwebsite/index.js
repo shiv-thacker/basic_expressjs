12adeb6496799f4be40685b62d6e6d4a592923ae
shivang <18ec.shivangthacker@gmail.com>
2023-06-27 13:21:36 +0530

    show static html, css website in home page

--- call_staticwebsite/index.js
+++ call_staticwebsite/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
-console.log(path.join(__dirname, "./staticwebsite"));
+console.log(path.join(__dirname, "./staticwebsite")); // here G:/sdcc/dcwsce/dcsd is absolute path & "../../wfcsdcfds" is relative path
 
 const staticpath = path.join(__dirname, "./staticwebsite");
 
-app.use(express.static(staticpath)); // to show direct static website in home page
+app.use(express.static(staticpath)); // to show direct static website in home page, express.static is built in middleware
 
 app.listen(port, () => {
   console.log(`listening to the port no ${port}`);
